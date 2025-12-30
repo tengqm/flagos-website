ef5206432933b8df70f52d1b5c32dd6e314b90af
Zhiqiang Zhao <learningambassador@163.com>
2025-12-30 18:38:56 +0800

    add index file and RTD yaml file for RTD project

---
+++ docs/conf.py
@@ -0,0 +1,167 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'test-sphinx'
+copyright = '2025, FlagOS Community'
+author = 'FlagOS Community'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+source_suffix = '.md'
+
+exclude_patterns = [
+    'README.md',      # 排除所有子目录中的 README.md
+    'README-en.md',   # 排除所有子目录中的 README.en.md
+]
+
+extensions = [
+    "myst_parser",
+    # "autodoc2",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx_design",
+    "sphinx_copybutton",
+    # "sphinxext.rediraffe",
+    # disabled due to https://github.com/mgaitan/sphinxcontrib-mermaid/issues/109
+    "sphinxcontrib.mermaid",
+    "sphinxext.opengraph",
+    "sphinx_pyscript",
+    "sphinx_tippy",
+    "sphinx_togglebutton",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+myst_enable_extensions = [
+    "dollarmath",
+    "amsmath",
+    "deflist",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "colon_fence",
+    "smartquotes",
+    "replacements",
+    # "linkify",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+    "attrs_inline",
+    "attrs_block",
+]
+myst_url_schemes = {
+    "http": None,
+    "https": None,
+    "mailto": None,
+    "ftp": None,
+    "wiki": "https://en.wikipedia.org/wiki/{{path}}#{{fragment}}",
+    "doi": "https://doi.org/{{path}}",
+    "gh-pr": {
+        "url": "https://github.com/executablebooks/MyST-Parser/pull/{{path}}#{{fragment}}",
+        "title": "PR #{{path}}",
+        "classes": ["github"],
+    },
+    "gh-issue": {
+        "url": "https://github.com/executablebooks/MyST-Parser/issue/{{path}}#{{fragment}}",
+        "title": "Issue #{{path}}",
+        "classes": ["github"],
+    },
+    "gh-user": {
+        "url": "https://github.com/{{path}}",
+        "title": "@{{path}}",
+        "classes": ["github"],
+    },
+}
+myst_number_code_blocks = ["typescript"]
+myst_heading_anchors = 2
+myst_footnote_transition = True
+myst_dmath_double_inline = True
+myst_enable_checkboxes = True
+myst_substitutions = {
+    "role": "[role](#syntax/roles)",
+    "directive": "[directive](#syntax/directives)",
+}
+
+
+
+# -- Options for sphinx-intl example
+
+locale_dirs = ['locale/']   # po files will be created in this directory
+gettext_compact = False     # optional: avoid file concatenation in sub directories.
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+# html_static_path = ['_static']
+
+html_theme = "sphinx_book_theme"
+html_logo = "_static/logo.svg"
+html_favicon = "_static/logo.svg"
+html_title = ""
+html_theme_options = {
+    "logo": {
+      "image_light": "_static/kernelgen-logo.svg",
+      "image_dark": "_static/kernelgen-logo.svg",
+   },
+    "home_page_in_toc": True,
+    "use_download_button": False,
+    "repository_url": "https://github.com/flagos-ai/KernelGen",
+    "use_edit_page_button": True,
+    # "github_url": "https://github.com/flagos-ai/KernelGen",
+    # "repository_branch": "master",
+    # "path_to_docs": "docs",
+    "use_repository_button": True,
+    # "announcement": "<b>v3.0.0</b> is now out! See the Changelog for details",
+}
+html_last_updated_fmt = ""
+# OpenGraph metadata
+ogp_site_url = "https://myst-parser.readthedocs.io/en/latest"
+# This is the image that GitHub stores for our social media previews
+ogp_image = "https://repository-images.githubusercontent.com/240151150/316bc480-cc23-11eb-96fc-4ab2f981a65d"
+ogp_custom_meta_tags = [
+    '<meta name="twitter:card" content="summary_large_image">',
+]
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+html_css_files = ["local.css"]
+
+rediraffe_redirects = {
+    "using/intro.md": "sphinx/intro.md",
+    "syntax/syntax.md": "syntax/typography.md",
+    "sphinx/intro.md": "intro.md",
+    "using/use_api.md": "api/index.md",
+    "api/index.md": "api/reference.rst",
+    "api/reference.rst": "apidocs/index.md",
+    "using/syntax.md": "syntax/syntax.md",
+    "using/syntax-optional.md": "syntax/optional.md",
+    "using/reference.md": "syntax/reference.md",
+    "sphinx/reference.md": "configuration.md",
+    "sphinx/index.md": "faq/index.md",
+    "sphinx/use.md": "faq/index.md",
+    "sphinx/faq.md": "faq/index.md",
+    "explain/index.md": "develop/background.md",
+}
+
+tippy_skip_anchor_classes = ("headerlink", "sd-stretched-link", "sd-rounded-pill")
+tippy_anchor_parent_selector = "article.bd-article"
+tippy_rtd_urls = [
+    "https://www.sphinx-doc.org/en/master",
+    "https://markdown-it-py.readthedocs.io/en/latest",
+]
+# TODO failing
+tippy_enable_wikitips = False
---
+++ docs/index.md
@@ -0,0 +1,95 @@
+---
+title: Welcome to My Landing Page
+site:
+  hide_outline: true
+  hide_toc: true
+  hide_title_block: true
+---
+
+# 文档中心
+
+```{note}
+欢迎来到我们的文档中心！在这里，您可以找到答案、获取分步指导，并学习如何使用我们的产品。
+```
+
+## 组成模块
+
+::::{grid} 1 2 3 3
+:gutter: 3
+
+:::{grid-item-card} FlagGems
+:link: 
+:link-type: url
+
+FlagGems 是一款使用Triton编程语言及其扩展语言实现的高性能通用算子库。旨在为大模型提供一系列通用算子，加速模型面向多种后端平台的推理与训练。
++++
+[了解更多](#)
+:::
+
+:::{grid-item-card} FlagTree
+:link: 
+:link-type: url
+
+FlagTree 是一款面向多种 AI 芯片的开源、统一编译器。致力于打造多元 AI 芯片编译器及相关工具平台，发展和壮大Triton 上下游生态, 目标是兼容现有适配方案，统一代码仓库，快速实现单仓库多后端支持。对于上游模型用户，FlagTree提供多后端的统一编译能力；对于下游芯片厂商，提供 Triton 生态接入范例。
++++
+[了解更多](#)
+:::
+
+:::{grid-item-card} FlagScale
+:link: 
+:link-type: url
+
+FlagScale 是一款全流程大模型工具集，可支持大模型的完整生命周期管理。该工具集依托 Megatron-LM 和 vLLM 等多款主流开源项目的技术优势，为大模型的管理与规模化部署提供了一套可靠的端到端解决方案。
++++
+[了解更多](#)
+:::
+
+:::{grid-item-card} FlagCX
+:link: https://www.redhat.com/en/technologies/artificial-intelligence
+:link-type: url
+
+FlagCX 是一款可扩展、自适应的跨芯片统一通信库。能面向多芯片、多平台场景，提供高性能的点对点与集合通信能力。在复用各平台原生集合通信能力的基础上，引入自主的设备缓冲区 IPC 与 RDMA 等技术，实现跨芯片与单芯片场景下的高效集合通信，并提供通信的自适应调优能力。
++++
+[了解更多](#)
+:::
+
+:::{grid-item-card} KernelGen
+:link: https://www.redhat.com/en/technologies/cloud-computing/openshift/virtualization
+:link-type: url
+
+KernelGen 是一款算子自动生成工具。旨在通过自然语言提示构建算子定义，检索现有相似算子定义，自动完成算子准确率及性能测试，生成准确率及性能测试结果，并生成Triton算子。
++++
+[了解更多](#)
+:::
+
+:::{grid-item-card} FlagRelease
+:text: md
+FlagRelease 是一款面向多架构人工智能芯片的大模型的自动迁移、适配与发布平台。旨在通过自动化、标准化和智能化的适配流程，使主流大模型能够在不同国产人工智能芯片上以更低成本、更高效率完成模型迁移、验证与发版。
++++
+[了解更多](#)
+:::
+
+:::{grid-item-card} FlagPerf
+:text: md
+FlagPerf是一款一体化AI硬件评测引擎。旨在建立以产业实践为导向的指标体系，评测AI硬件在软件栈组合（模型+框架+编译器）下的实际能力。
++++
+[了解更多](#)
+:::
+
+::::
+
+
+## 关于文档中心
+
+我们通过可信的内容帮助用户创新并实现其目标。探索我们的最新更新。
+
+```{admonition} 使开源更具包容性
+:class: tip
+
+我们致力于替换代码、文档和Web属性中的有问题的语言。
+```
+
+### 关于我们
+
+
+---
